1e615db80ee5aeb16d28597a3addcfe2c8cf5276
Renat Yagudin <9914030@gmail.com>
2023-09-20 13:38:29 +0000

    Added files

---
+++ files/app/index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="author" content="Steven Liebregt" />
+        <title>Hello, world!</title>
+        <style>
+            table, th, td {
+                border: 1px solid black;
+                border-collapse: collapse;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Congratulations! Renat, your code works!!@@@</h1>
+        <p>If you can see this properly, then it means PHP is working fine.</p>
+    </body>
+</html>
